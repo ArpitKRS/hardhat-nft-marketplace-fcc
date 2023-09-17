ad84e1f603ff1550f6003b291ce3561af140f931
Arpit Kumar Singh <singharpit302@gmail.com>
2023-09-18 00:25:07 +0530

    buyItem function setup (Maybe complete)

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -8,14 +8,25 @@ error NftMarketplace__PriceMustBeAboveZero();
 error NftMarketplace__NotApprovedForMarketplace();
 error NftMarketplace__AlreadyListed(address nftAddress, uint256 tokenId);
 error NftMarketplace__NotOwner();
+error NftMarketplace__NotListed(address nftAddress, uint256 tokenId);
+error NftMarketplace__PriceNotMet(address nftAddress, uint256 tokenId, uint256 price);
 
 contract NftMarketplace {
+
     struct Listing {
         uint256 price;
         address seller;
     }
+
+    // Events
     event ItemListed(
         address indexed seller,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+    event ItemBought(
+        address indexed buyer,
         address indexed nftAddress,
         uint256 indexed tokenId,
         uint256 price
@@ -23,6 +34,9 @@ contract NftMarketplace {
 
     // NFT contract address -> NFT tokenId -> Listing
     mapping(address => mapping(uint256 => Listing)) private s_listings;
+
+    // Seller address -> Amount earned
+    mapping(address => uint256) private s_proceeds;
 
     // Modifier
     modifier notListed(
@@ -45,6 +59,15 @@ contract NftMarketplace {
         IERC721 nft = IERC721(nftAddress);
         address owner = nft.ownerOf(tokenId);
         if (spender != nft.ownerOf(tokenId)) revert NftMarketplace__NotOwner();
+        _;
+    }
+
+    modifier isListed(
+        address nftAddress,
+        uint256 tokenId
+    ) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+        if(listing.price <= 0) revert NftMarketplace__NotListed(nftAddress, tokenId);
         _;
     }
 
@@ -71,5 +94,24 @@ contract NftMarketplace {
             revert NftMarketplace__NotApprovedForMarketplace();
         s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
         emit ItemListed(msg.sender, nftAddress, tokenId, price);
+    }
+
+    function buyItem(address nftAddress, uint256 tokenId) 
+        external
+        payable
+        isListed(nftAddress, tokenId)
+    {
+        Listing memory listedItem = s_listings[nftAddress][tokenId];
+        IERC721(nftAddress).safeTransferFrom(listedItem.seller, msg.sender, tokenId);
+
+        if (msg.value < listedItem.price) revert NftMarketplace__PriceNotMet(nftAddress, tokenId, listedItem.price);
+        // We don't just send the seller the money
+        // sending the money to the user can be harmful so...
+        // Have them withdraw the money instead
+        s_proceeds[listedItem.seller] += msg.value;
+        delete (s_listings[nftAddress][tokenId]);
+        
+        // checks to make sure the NFT was transferred
+        emit ItemBought(msg.sender, nftAddress, tokenId, listedItem.price);
     }
 }
